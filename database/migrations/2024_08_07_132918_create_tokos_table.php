f170bb156e26eeb4409152623220fc35542eebf2
didanboy51 <didanboy51@gmail.com>
2024-08-08 07:58:24 +0700

    toko

--- database/migrations/2024_08_07_132918_create_tokos_table.php
+++ database/migrations/2024_08_07_132918_create_tokos_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -17,6 +18,7 @@ return new class extends Migration
             $table->string("deskripsi");
             $table->string("lokasi");
             $table->string("alamat");
+            $table->foreignIdFor(User::class)->constrained();
             $table->enum("kategori_toko", ["jasa", "barang"]);
             $table->timestamps();
         });

643deed2930a6580ff5855927e60c8086d778dab
muhammadsaugi <muhammadsaugi7@gmail.com>
2024-08-08 09:40:17 +0700

    first commit

---
+++ resources/views/admin/pesanan/index.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid">
+    <!-- Content Row -->
+        <div class="card">
+            <div class="card-header py-3 d-flex">
+                <h6 class="m-0 font-weight-bold text-gray">
+                    {{ __('Product Data') }}
+                </h6>
+                <div class="ml-auto">
+                    <a href="/" class="btn btn-success">
+                        <span class="icon text-white-50">
+                            <i class="fa fa-plus"></i>
+                        </span>
+                        <span class="text">{{ __('New product') }}</span>
+                    </a>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table id="data-table" class="table table-bordered table-striped table-hover datatable datatable-product" cellspacing="0" width="100%">
+                        <thead>
+                            <tr>
+                                <th width="10">
+
+                                </th>
+                                <th>No</th>
+                                <th>Nama Customer</th>
+                                <th>Total Harga</th>
+                                <th>Jasa</th>
+                                <th>Status</th>
+                                <th>Action</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    <!-- Content Row -->
+
+</div>
+@endsection
+
+
+@push('style-alt')
+  <!-- DataTables -->
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.3/css/jquery.dataTables.min.css">
+@endpush
+
+@push('script-alt')
+    <script
+        src="https://code.jquery.com/jquery-3.6.3.min.js"
+        integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU="
+        crossorigin="anonymous"
+    >
+    </script>
+    <script src="https://cdn.datatables.net/1.13.3/js/jquery.dataTables.min.js"></script>
+    <script>
+    $("#data-table").DataTable();
+    </script>
+@endpush
